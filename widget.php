ab63d36c1bc55cf10d77a62c8036e3175c30ada8
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-02-22 16:20:00 +0000

    - Add related apps to the widget.

--- widget.php
+++ widget.php
@@ -23,42 +23,59 @@ class OSIntegration_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		extract( $args );
-		
+
 		$options = get_option( OSINTOPTIONNAME );
 
 		// If at least one of the options is enabled, output the widget.
-		if( ( array_key_exists( 'enablefavicon', $options ) && $options['enablefavicon'] ) || 
-			( array_key_exists( 'enablelivetile', $options ) && $options['enablelivetile'] ) || 
-			( array_key_exists( 'enableios', $options ) && $options['enableios'] ) || 
+		if( ( array_key_exists( 'enablefavicon', $options ) && $options['enablefavicon'] ) ||
+			( array_key_exists( 'enablelivetile', $options ) && $options['enablelivetile'] ) ||
+			( array_key_exists( 'enableios', $options ) && $options['enableios'] ) ||
 			( array_key_exists( 'enablewebapp', $options ) && $options['enablewebapp'] ) ) {
 			$title = __( 'Bookmark us!', 'os-integration' );
 			$pin      = __( 'Bookmark or Pin us!', 'os-integration' );
-			
-			
-			if( array_key_exists( 'enablelivetile', $options ) && $options['enablelivetile'] || $options['enableios'] || $options['enablewebapp'] || $options['enablepwa'] ) { 
+
+
+			if( osintegration_getoption( 'enablelivetile', $options )
+				|| osintegration_getoption( 'enableios', $options )
+				|| osintegration_getoption( 'enablewebapp', $options )
+				|| osintegration_getoption( 'enablepwa', $options ) ) {
 				$title = __( 'Bookmark or Pin us!', 'os-integration' );
 			}
-			
+
 			echo $before_widget;
 			echo $before_title . $title . $after_title;
-						
+
 			echo '<p>' . sprintf( __( '%s supports many popular operating systems!', 'os-integraton' ), get_bloginfo( 'name' ) ) . '</p>';
 			echo '<p>' . __( 'Supported platforms include:', 'os-integration' ) . '</p>';
 			echo '<ul>';
-			
+
 			if( array_key_exists( 'enablefavicon', $options ) && $options['enablefavicon'] ) { echo '<li>' . __( 'FavIcons for desktop and mobile browsers', 'os-integration' ) . '</li>'; }
 			if( array_key_exists( 'enablepwa', $options ) && $options['enablefavicon'] ) { echo '<li>' . __( 'Progressive Web Apps for desktop and mobile browsers', 'os-integration' ) . '</li>'; }
 			if( array_key_exists( 'enablelivetile', $options ) && $options['enablelivetile'] ) { echo '<li>' . __( 'Windows 8 and Windows Phone 8.1 Live Tiles', 'os-integration' ) . '</li>'; }
 			if( array_key_exists( 'enableios', $options ) && $options['enableios'] ) { echo '<li>' . __( 'iOS Home Screen Icons', 'os-integration' ) . '</li>'; }
 			if( array_key_exists( 'enablewebapp', $options ) && $options['enablewebapp'] ) { echo '<li>' . __( 'iOS WebApp', 'os-integration' ) . '</li>'; }
-			
+
+			$store_list = '';
+
+			foreach( osintegration_store_list() as $store => $name ) {
+				$ra_url = osintegration_getoption( $store . 'url', $options );
+
+				if( $ra_url ) {
+					$store_list .= '<a href="' . $ra_url . '"><img src="' . plugins_url( 'os-integration/images/' . $store . '.png' ) . '"></a>&nbsp;';
+				}
+			}
+
+			if( $store_list !== '' ) {
+				echo '<li>' . $store_list . '</li>';
+			}
+
 			echo '</ul>';
-			
+
 			echo $after_widget;
 		}
 	}
-}	
+}
 
 add_action( 'widgets_init', 'os_integraton_widget' );
-	
+
 ?>
